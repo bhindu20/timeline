b302123e3aa42667c686841413df9c2572e42361
Marie Selvanadin <bhindu20@gmail.com>
2012-01-22 20:17:17 -0500

    timeglider files

--- cndlstimeline/urls.py
+++ cndlstimeline/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^(?P<user_id>\d+)/(?P<timeline_id>\d+)/$', 'cndlstimeline.views.display'),
-    url(r'^get-timeline/$', 'cndlstimeline.views.get_timeline')
 )
